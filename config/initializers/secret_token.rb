67acced8e27b1ffc64fc1e74ba70657975102691
Kurt Sang Hyun Lee <kurt@vingle.net>
2014-05-18 00:47:57 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Napkins::Application.config.secret_key_base = '4de48d165b7d02d785def122375abdb77340e481dc565359f41f48fd7594e1d9533c122d8a2d5ccf7fea68d1677aa736fc5f2773afb3d55c640ad9c3de5337f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Napkins::Application.config.session_store :cookie_store, key: '_napkins_session'
